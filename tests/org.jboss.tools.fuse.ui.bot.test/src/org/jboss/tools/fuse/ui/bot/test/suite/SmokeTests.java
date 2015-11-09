278f23bbd0d49aa1e7fc578d0d91341858f2496c
Tomáš Sedmík <tsedmik@redhat.com>
2015-11-09 09:36:01 +0100

    Fuse: Improve stability of suites #243

--- tests/org.jboss.tools.fuse.ui.bot.test/src/org/jboss/tools/fuse/ui/bot/test/suite/SmokeTests.java
+++ tests/org.jboss.tools.fuse.ui.bot.test/src/org/jboss/tools/fuse/ui/bot/test/suite/SmokeTests.java
@@ -1,7 +1,9 @@
 package org.jboss.tools.fuse.ui.bot.test.suite;
 
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.fuse.ui.bot.test.JMXNavigatorTest;
 import org.jboss.tools.fuse.ui.bot.test.ProjectLocalRunTest;
+import org.jboss.tools.fuse.ui.bot.test.ServerTest;
 import org.jboss.tools.fuse.ui.bot.test.SmokeTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
@@ -15,7 +17,9 @@ import junit.framework.TestSuite;
  */
 @SuiteClasses({
 		ProjectLocalRunTest.class,
-		SmokeTest.class
+		SmokeTest.class,
+		ServerTest.class,
+		JMXNavigatorTest.class
 })
 @RunWith(RedDeerSuite.class)
 public class SmokeTests extends TestSuite {
